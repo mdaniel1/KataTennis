1a1de6302dff8255853a72cda135e04e5a10cf13
mdaniel1 <martin.daniel.noel@gmail.com>
2019-07-17 17:11:15 +0200

    fixed gitignore and removed useless files from github

--- PongKata/Classes/Player.cs
+++ PongKata/Classes/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,6 +26,47 @@ namespace PongKata.Classes
         public Player(PictureBox paddle)
         {
             this.paddle = paddle;
+        }
+
+        public void processMovePaddle()
+        {
+            bool? paddleGoingUp = null; //null means the paddle is not moving
+
+            if (goingUp)
+                paddleGoingUp = true;
+
+            if (goingDown)
+            {
+                if (paddleGoingUp.HasValue)
+                {
+                    paddleGoingUp = null; //If we're already pressing up, cancel the movement
+                }
+                else
+                {
+                    paddleGoingUp = false;
+                }
+            }
+
+            movePaddle(paddleGoingUp);
+        }
+
+        private void movePaddle(bool? paddleGoingUp)
+        {
+            int movementSpeed = PongKataForm.PADDLE_SPEED + acceleration;
+
+            if (paddleGoingUp.HasValue) //Value is not null
+            {
+                if (paddleGoingUp.Value) //And true
+                {
+                    movementSpeed *= -1;
+                }
+
+                //Sets the new location to a new one without going off limits
+                paddle.Location = new Point(paddle.Location.X,
+                    Math.Max(PongKataForm.TOP_WINDOW_LOCATION,
+                    Math.Min(PongKataForm.BOTTOM_WINDOW_LOCATION, paddle.Location.Y + movementSpeed))
+                    );
+            }
         }
     }
 }
--- PongKata/GUI/PongKataForm.cs
+++ PongKata/GUI/PongKataForm.cs
@@ -15,10 +15,10 @@ namespace PongKata
     {
 
         #region VARIABLES
-        const int PADDLE_SPEED = 5;
-        const int PADDLE_ACCEL = 5;
-        const int TOP_WINDOW_LOCATION = 0;
-        const int BOTTOM_WINDOW_LOCATION = 534;
+        public const int PADDLE_SPEED = 5;
+        public const int PADDLE_ACCEL = 5;
+        public const int TOP_WINDOW_LOCATION = 0;
+        public const int BOTTOM_WINDOW_LOCATION = 534;
 
         Player player1; 
         Player player2;
@@ -33,49 +33,8 @@ namespace PongKata
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-            processMovePaddle(player1, player1.goingUp, player1.goingDown);
-            processMovePaddle(player2, player2.goingUp, player2.goingDown);
-        }
-
-        private void processMovePaddle(Player player, bool isGoingUp, bool isGoingDown)
-        {
-            bool? paddleGoingUp = null; //null means the paddle is not moving
-
-            if (isGoingUp)
-                paddleGoingUp = true;
-
-            if (isGoingDown)
-            {
-                if (paddleGoingUp.HasValue)
-                {
-                    paddleGoingUp = null; //If we're already pressing up, cancel the movement
-                }
-                else
-                {
-                    paddleGoingUp = false;
-                }
-            }
-
-            movePaddle(paddleGoingUp, player);
-        }
-
-        private void movePaddle(bool? paddleGoingUp, Player player)
-        {
-            int movementSpeed = PADDLE_SPEED + player.acceleration;
-
-            if (paddleGoingUp.HasValue) //Value is not null
-            {
-                if (paddleGoingUp.Value) //And true
-                {
-                    movementSpeed *= -1;
-                }
-
-                //Sets the new location to a new one without going off limits
-                player.paddle.Location = new Point(player.paddle.Location.X, 
-                    Math.Max(TOP_WINDOW_LOCATION, 
-                    Math.Min(BOTTOM_WINDOW_LOCATION, player.paddle.Location.Y + movementSpeed))
-                    );
-            }
+            player1.processMovePaddle();
+            player2.processMovePaddle();
         }
 
         private void PongKataForm_KeyDown(object sender, KeyEventArgs e)
@@ -99,6 +58,8 @@ namespace PongKata
                 case Keys.S:
                     player1.goingDown = isKeyPressed;
                     break;
+                case Keys.LShiftKey: //I don't really know why only ShiftKey is recognized, probably a matter of keyboard, so I'm putting all the possibilities for the left shift key just in case
+                case Keys.Shift:
                 case Keys.ShiftKey:
                     if (isKeyPressed)
                     {
